80c0a4057f6abb37d4ced29a13cfa700f4aa791f
Zhenhan-Lin <116985151+Zhenhan-Lin@users.noreply.github.com>
2023-02-24 16:22:31 -0500

    modify transfer.c

--- EFT program/inc/transfer.c
+++ EFT program/inc/transfer.c
@@ -11,16 +11,11 @@
 
 char *pattern_2 = "^Transfer ([0-9]+) ([0-9]+) ([0-9]+)";
 
-void update_account(size_t account_number, long amount) {
+Account get_account(size_t account_number) {
     // Find account
-    Account a;
-    for (size_t i = 0; (a = &(acc_list[i]))->number != account_number; i++);
-    // Lock
-    pthread_mutex_lock(a->lock);
-    // Update balance
-    a->balance += amount;
-    // Unlock
-    pthread_mutex_unlock(a->lock);
+    size_t i = 0;
+    while (acc_list[i].number != account_number) i++;
+    return &(acc_list[i]);
 }
 
 void parse_and_execute(char * s) {
@@ -35,8 +30,20 @@ void parse_and_execute(char * s) {
     receive = atol(strsep(&s, " "));
     amount = atol(strsep(&s, " "));
 
-    update_account(draw, -amount);
-    update_account(receive, amount);
+    if (draw == receive) return;
+
+    Account
+        a = get_account(draw),
+        b = get_account(receive);
+    pthread_mutex_lock(a->lock);
+    while (pthread_mutex_trylock(b->lock) != 0)
+        pthread_cond_wait(b->cond, a->lock);
+    a->balance -= amount;
+    b->balance += amount;
+    pthread_mutex_unlock(b->lock);
+    pthread_cond_broadcast(b->cond);
+    pthread_mutex_unlock(a->lock);
+    pthread_cond_broadcast(a->cond);
 }
 
 static pthread_mutex_t read_lock = PTHREAD_MUTEX_INITIALIZER;
